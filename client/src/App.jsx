553ed42c89fcc8db18b803b54db52356d42ae2e4
Antoine Chassagne <antoine.chassagne@me.com>
2020-06-22 11:17:09 +0200

    Add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-  return <h1>Bienvenue sur Gonia</h1>;
-}
+const App = () => {
+  console.log(process.env.PUBLIC_URL);
+  return (
+    <Router>
+      <main className="container">
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
