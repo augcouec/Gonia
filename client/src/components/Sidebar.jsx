864f645c622ed5d99cf0d4501f91164d05e50681
Antoine Chassagne <antoine.chassagne@me.com>
2020-06-23 18:42:35 +0200

    add active link sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import AuthenticationManager from "../services/AuthenticationManager";
 
 const Sidebar = () => {
+  const role = AuthenticationManager.getRole();
   return (
     <aside className="sidebar">
       <span className="sidebar__brand">Gonia</span>
       <ul>
         <li>
-          <a href="/dashboard">Home</a>
+          <NavLink to="/dashboard" activeClassName="active">
+            Home
+          </NavLink>
         </li>
         <li>
-          <a href="/projects">Commandes</a>
+          <NavLink to="/projects" activeClassName="active">
+            Commandes
+          </NavLink>
         </li>
         <li>
           <a className="disabled">Facturation</a>
