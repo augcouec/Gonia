b2c987448d4a05019ee030aaf9ee6f5d822c9a9d
Antoine Chassagne <antoine.chassagne@me.com>
2020-06-24 01:25:45 +0200

    fix post routes on api

--- api/src/router.js
+++ api/src/router.js
@@ -9,7 +9,7 @@ const authenticationController = require("./controllers/authentication");
 router.get("/users/count", userController.getUsersCount);
 router.get("/users/:id", userController.getUser);
 router.get("/users", userController.getUsers);
-router.post("/users/create", userController.createUser);
+router.post("/users", userController.createUser);
 router.put("/users/:id", userController.updateUser);
 router.delete("/users/:id", userController.deleteUser);
 
@@ -17,7 +17,7 @@ router.delete("/users/:id", userController.deleteUser);
 router.get("/projects/count", projectController.getProjectsCount);
 router.get("/projects/:id", projectController.getProject);
 router.get("/projects", projectController.getProjects);
-router.post("/projects/create", projectController.createProject);
+router.post("/projects", projectController.createProject);
 router.put("/projects/:id", projectController.updateProject);
 router.delete("/projects/:id", projectController.deleteProject);
 
