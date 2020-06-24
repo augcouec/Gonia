fcca05c67cfa832f5e4b3a55f59707d49f7a17d4
Antoine Chassagne <antoine.chassagne@me.com>
2020-06-24 15:44:17 +0200

    Add dynamic API route

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://127.0.0.1:8000/api"
+      : "https://gonia.herokuapp.com/api",
 });
 
 export default Api;
