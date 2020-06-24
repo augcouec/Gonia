1b54aef3012ac8d9c6aa0513726ac5184ab70dfe
Antoine Chassagne <antoine.chassagne@me.com>
2020-06-24 16:06:03 +0200

    fix port

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const Api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:8000/api"
+      ? "http://127.0.0.1:8080/api"
       : "https://gonia.herokuapp.com/api",
 });
 
