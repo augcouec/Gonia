c9b98047ca1f4c5ddbdc1afa15b79c2764113592
Antoine Chassagne <antoine.chassagne@me.com>
2020-06-24 21:20:44 +0200

    Add way to get current user

--- client/src/services/AuthenticationManager.js
+++ client/src/services/AuthenticationManager.js
@@ -10,4 +10,7 @@ export default {
     const user = JSON.parse(localStorage.getItem("user"));
     return user ? user._id : null;
   },
+  getUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  },
 };
